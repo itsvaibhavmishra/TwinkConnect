dfbc7573260681c8d899171651403ec6c312898e
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-07-06 14:20:17 +0530

    Installed modules, setup scrollbar, loading screen, etc

---
+++ client/src/components/Iconify.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+import { Icon } from '@iconify/react';
+
+Iconify.propTypes = {
+  icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  sx: PropTypes.object,
+};
+
+export default function Iconify({ icon, sx, ...other }) {
+  return <Box component={Icon} icon={icon} sx={{ ...sx }} {...other} />;
+}
---
+++ client/src/components/LoadingScreen.js
@@ -0,0 +1,5 @@
+const LoadingScreen = () => {
+  return <>Loading...</>;
+};
+
+export default LoadingScreen;
