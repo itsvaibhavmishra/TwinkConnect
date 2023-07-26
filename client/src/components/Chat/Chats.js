f913770776f268ef4578e0abdc8f2a5e6af15de3
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-07-26 10:53:45 +0530

    Group chat left panel complete

--- client/src/components/Chat/Chats.js
+++ client/src/components/Chat/Chats.js
@@ -3,51 +3,15 @@ import {
   Button,
   Divider,
   IconButton,
-  InputBase,
   Stack,
   Typography,
-  alpha,
-  styled,
   useTheme,
 } from '@mui/material';
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from 'phosphor-react';
 import React from 'react';
 import { ChatList } from '../../data';
 import ChatElement from './ChatElement';
-
-// styles for search bar
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: 20,
-  backgroundColor: alpha(
-    theme.palette.mode === 'light' ? '#F0F4FF' : theme.palette.background.paper,
-    1
-  ),
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-}));
-
-// styles for search icon
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-// implementation of search functionality
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    width: '100%',
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-  },
-}));
+import { Search, SearchIconWrapper, StyledInputBase } from '../Search';
 
 const Chats = () => {
   // using theme
--- client/src/components/Group/Groups.js
+++ client/src/components/Group/Groups.js
@@ -1,5 +1,17 @@
-import { Box, useTheme } from '@mui/material';
+import {
+  Box,
+  Divider,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import { CircleDashed, MagnifyingGlass, Plus } from 'phosphor-react';
 import React from 'react';
+import { Search, SearchIconWrapper, StyledInputBase } from '../Search';
+import { ChatList } from '../../data';
+import ChatElement from '../Chat/ChatElement';
 
 const Groups = () => {
   // using theme
@@ -14,7 +26,97 @@ const Groups = () => {
         backgroundColor: theme.palette.background.default,
         boxShadow: '0px 0px 2px #00000040',
       }}
-    ></Box>
+    >
+      {/* Header */}
+      <Stack p={3} spacing={2} sx={{ maxHeight: '100vh' }}>
+        <Stack
+          direction={'row'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+        >
+          <Typography variant="h5">Groups</Typography>
+          <IconButton>
+            <CircleDashed />
+          </IconButton>
+        </Stack>
+
+        {/* Search stack */}
+        <Stack sx={{ width: '100%' }}>
+          <Search>
+            <SearchIconWrapper>
+              <MagnifyingGlass color="#709CE6" />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Stack>
+
+        {/* Create new group stack */}
+        <Stack
+          direction={'row'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+        >
+          <Typography variant="subtitle2" component={Link}>
+            Create New Group
+          </Typography>
+          <IconButton>
+            <Plus style={{ color: theme.palette.primary.main }} />
+          </IconButton>
+        </Stack>
+        <Divider />
+
+        {/* Group Chat section starts here */}
+        <Stack
+          direction={'column'}
+          sx={{
+            flexGrow: 1,
+            overflow: 'scroll',
+            height: '100%',
+            overflowY: 'auto',
+            overflowX: 'hidden',
+          }}
+          spacing={2}
+          className="scrollbar"
+        >
+          {/* Pinned Group Messages */}
+          <Stack spacing={2.4}>
+            <Typography variant="subtitle2" sx={{ color: '#676767' }}>
+              Pinned
+            </Typography>
+            <Stack
+              sx={{
+                backgroundColor: theme.palette.background.paper,
+                borderRadius: 1,
+              }}
+            >
+              {ChatList.filter((e) => e.pinned).map((e) => {
+                return <ChatElement {...e} />;
+              })}
+            </Stack>
+          </Stack>
+
+          {/* All Group Chats */}
+          <Stack spacing={2.4}>
+            <Typography variant="subtitle2" sx={{ color: '#676767' }}>
+              All Groups
+            </Typography>
+            <Stack
+              sx={{
+                backgroundColor: theme.palette.background.paper,
+                borderRadius: 1,
+              }}
+            >
+              {ChatList.filter((e) => !e.pinned).map((e) => {
+                return <ChatElement {...e} />;
+              })}
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
   );
 };
 
