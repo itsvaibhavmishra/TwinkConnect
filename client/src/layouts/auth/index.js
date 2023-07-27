1baf1c85e702c6f9025d4916444d47341ce9c226
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-07-27 18:38:04 +0530

    Added dummy protected route

--- client/src/layouts/auth/index.js
+++ client/src/layouts/auth/index.js
@@ -1,19 +1,25 @@
-import { Container, Stack } from '@mui/material';
-import { Outlet } from 'react-router-dom';
-import Logo from '../../assets/Images/TwinkChat.png';
+import { Container, Stack } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
+import Logo from "../../assets/Images/TwinkChat.png";
+
+const isAuthenticated = true;
 
 const AuthLayout = () => {
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <Container sx={{ mt: 5 }} maxWidth="sm">
       <Stack spacing={5}>
         <Stack
-          direction={'column'}
-          alignItems={'center'}
-          sx={{ width: '100%' }}
+          direction={"column"}
+          alignItems={"center"}
+          sx={{ width: "100%" }}
         >
           <img
             src={Logo}
-            alt={'TwinkChat Logo'}
+            alt={"TwinkChat Logo"}
             style={{ height: 120, width: 120 }}
           />
         </Stack>
