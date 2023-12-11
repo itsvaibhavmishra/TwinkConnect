d6b83c6367748f0f320c31c289abac328b547678
Vaibhaw Mishra <vaibhavmishra658@gmail.com>
2023-12-11 14:01:22 +0530

    Added Uncaught Exception, Unhandled Rejection and SIGTER errors | updated .env

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,31 @@ const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Server on port ${port}`);
 });
+
+// ---------Handling server errors---------
+const exitHandler = () => {
+  if (server) {
+    console.log("Closing Server...");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.log(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// SIGTERM Handling - (works for deployed linux based server)
+process.on("SIGTERM", () => {
+  if (server) {
+    console.log("Closing Server...");
+    process.exit(1);
+  }
+});
+
+// ---------------------------------------
