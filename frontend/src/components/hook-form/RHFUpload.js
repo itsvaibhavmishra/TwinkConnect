31536459389fe58657e3e67d9b3b56ab5fc8d112
Vaibhaw Mishra <vaibhavmishra658@gmail.com>
2023-12-26 10:04:01 +0530

    Image Cropper added

--- frontend/src/components/hook-form/RHFUpload.js
+++ frontend/src/components/hook-form/RHFUpload.js
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 import { useFormContext, Controller } from "react-hook-form";
 import { FormHelperText } from "@mui/material";
-
 import { UploadAvatar } from "../upload";
+import AvatarCropper from "../upload/preview/AvatarCropper";
+import { useEffect, useState } from "react";
 
 RHFUploadAvatar.propTypes = {
   name: PropTypes.string,
@@ -11,33 +12,66 @@ RHFUploadAvatar.propTypes = {
 };
 
 export function RHFUploadAvatar({ name, formState, onRemove, ...other }) {
-  const { control } = useFormContext();
+  const methods = useFormContext(); // Use useFormContext to get the form methods
+
+  const [cropperOpen, setCropperOpen] = useState(false);
+
+  const handleOpenCropper = () => {
+    setCropperOpen(true);
+  };
+
+  const handleCloseCropper = () => {
+    setCropperOpen(false);
+  };
+
+  const handleUseCroppedImage = async (croppedImage) => {
+    methods.setValue(name, croppedImage);
+    handleCloseCropper();
+  };
+
+  useEffect(() => {
+    const imageValue = methods.getValues(name);
+    if (imageValue && cropperOpen) {
+      handleOpenCropper();
+    }
+  }, [name, methods, cropperOpen]);
 
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, fieldState: { error } }) => (
-        <div>
-          <UploadAvatar
-            accept={{
-              "image/*": [],
-            }}
-            error={!!error}
-            file={field.value}
-            onRemove={onRemove}
-            formState={formState}
-            {...other}
-          />
+    <div>
+      <Controller
+        name={name}
+        control={methods.control}
+        render={({ field, fieldState: { error } }) => (
+          <div>
+            {cropperOpen && (
+              <AvatarCropper
+                open={cropperOpen}
+                handleClose={handleCloseCropper}
+                image={field.value}
+                onUse={handleUseCroppedImage}
+              />
+            )}
+            <UploadAvatar
+              accept={{
+                "image/*": [],
+              }}
+              error={!!error}
+              file={field.value}
+              onRemove={onRemove}
+              formState={formState}
+              onCrop={handleOpenCropper}
+              {...other}
+            />
 
-          {!!error && (
-            <FormHelperText error sx={{ px: 2, textAlign: "center" }}>
-              {(!formState.isSubmitSuccessful || formState.isDirty) &&
-                error.message}
-            </FormHelperText>
-          )}
-        </div>
-      )}
-    />
+            {!!error && (
+              <FormHelperText error sx={{ px: 2, textAlign: "center" }}>
+                {(!formState.isSubmitSuccessful || formState.isDirty) &&
+                  error.message}
+              </FormHelperText>
+            )}
+          </div>
+        )}
+      />
+    </div>
   );
 }
--- frontend/src/components/upload/index.js
+++ frontend/src/components/upload/index.js
@@ -1,2 +1,3 @@
 export { default as AvatarPreview } from "./preview/AvatarPreview";
+export { default as AvatarCropper } from "./preview/AvatarCropper";
 export { default as UploadAvatar } from "./UploadAvatar";
