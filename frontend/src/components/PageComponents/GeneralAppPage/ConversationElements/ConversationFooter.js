21143f9625562148dc62b864e9231f613df2103a
Vaibhaw Mishra <vaibhavmishra658@gmail.com>
2024-01-29 23:18:24 +0530

    Conversation Footer UI complete:

--- frontend/src/components/PageComponents/GeneralAppPage/ConversationElements/ConversationFooter.js
+++ frontend/src/components/PageComponents/GeneralAppPage/ConversationElements/ConversationFooter.js
@@ -1,18 +1,121 @@
-import { Box, useTheme } from "@mui/material";
+import { useRef, useState, useEffect } from "react";
+import { Box, Stack, IconButton, useTheme, Grow } from "@mui/material";
+import { PaperPlaneTilt } from "phosphor-react";
+
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
+import ChatInput from "./ConvoSubElements/ChatInput";
 
 const ConversationFooter = () => {
   const theme = useTheme();
 
+  const [value, setValue] = useState("");
+  const [openPicker, setOpenPicker] = useState(false);
+  const inputRef = useRef(null);
+  const pickerRef = useRef(null);
+
+  const handleEmojiClick = (emoji) => {
+    const input = inputRef.current;
+
+    if (input) {
+      const selectionStart = input.selectionStart;
+      const selectionEnd = input.selectionEnd;
+
+      setValue(
+        value.substring(0, selectionStart) +
+          emoji +
+          value.substring(selectionEnd)
+      );
+
+      // Move the cursor to the end of the inserted emoji
+      input.selectionStart = input.selectionEnd = selectionStart + 1;
+    }
+  };
+
+  const handleClickOutsidePicker = (event) => {
+    if (pickerRef.current && !pickerRef.current.contains(event.target)) {
+      setOpenPicker(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (value && value !== "") {
+      console.log(value);
+      // Clear the input field
+      setValue("");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsidePicker);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsidePicker);
+    };
+  }, []);
+
   return (
     <Box
-      p={2}
+      py={2}
+      px={3}
+      width={"100%"}
       sx={{
-        height: 100,
         backgroundColor: theme.palette.background.default,
+        boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
       }}
     >
-      ConversationFooter
+      <form onSubmit={handleSubmit}>
+        <Stack direction="row" alignItems={"center"} spacing={2}>
+          <Stack sx={{ width: "100%" }}>
+            <Grow in={openPicker}>
+              <Box
+                ref={pickerRef}
+                sx={{
+                  zIndex: 10,
+                  position: "fixed",
+                  bottom: { xs: 80, md: 65 },
+                  right: { xs: 5, sm: 80, md: 100 },
+                }}
+              >
+                <Picker
+                  perLine={8}
+                  theme={theme.palette.mode}
+                  data={data}
+                  onEmojiSelect={(emoji) => {
+                    handleEmojiClick(emoji.native);
+                  }}
+                />
+              </Box>
+            </Grow>
+            {/* Chat Input */}
+            <ChatInput
+              openPicker={openPicker}
+              setOpenPicker={setOpenPicker}
+              inputRef={inputRef}
+              value={value}
+              setValue={setValue}
+              theme={theme}
+            />
+          </Stack>
+
+          <IconButton
+            type="submit"
+            sx={{
+              height: 40,
+              width: 40,
+              backgroundColor: theme.palette.primary.main,
+              borderRadius: 20,
+              transition: "background-color 0.2s ease",
+            }}
+          >
+            <PaperPlaneTilt color="#ffffff" size={20} />
+          </IconButton>
+        </Stack>
+      </form>
     </Box>
   );
 };
+
 export default ConversationFooter;
